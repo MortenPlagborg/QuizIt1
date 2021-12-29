64bedeb0c279b7d7838b478d30b63681b32bf531
MortenPlagborg <morten.plagborg@hotmail.com>
2021-12-29 23:33:02 +0100

    hger

--- app/src/main/java/com/example/quizit/ListFragment.java
+++ app/src/main/java/com/example/quizit/ListFragment.java
@@ -2,16 +2,38 @@ package com.example.quizit;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ProgressBar;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ListFragment extends Fragment {
+public class ListFragment extends Fragment implements QuizListAdapter.OnQuizListItemClicked {
+
+    private NavController navController;
+    private RecyclerView listView;
+    private QuizListViewModel quizListViewModel;
+
+    private QuizListAdapter adapter;
+    private ProgressBar listProgress;
+    private Animation fadeInAnim;
+    private Animation fadeOutAnim;
 
     public ListFragment() {
         // Required empty public constructor
@@ -22,5 +44,47 @@ public class ListFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        navController = Navigation.findNavController(view);
+        listView = view.findViewById(R.id.list_view);
+        listProgress = view.findViewById(R.id.list_progress);
+        adapter = new QuizListAdapter(this);
+
+        listView.setLayoutManager(new LinearLayoutManager(getContext()));
+        listView.setHasFixedSize(true);
+        listView.setAdapter(adapter);
+
+        fadeInAnim = AnimationUtils.loadAnimation(getContext(), R.anim.fade_in);
+        fadeOutAnim = AnimationUtils.loadAnimation(getContext(), R.anim.fade_out);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        quizListViewModel = new ViewModelProvider(getActivity()).get(QuizListViewModel.class);
+        quizListViewModel.getQuizListModelData().observe(getViewLifecycleOwner(), new Observer<List<QuizListModel>>() {
+            @Override
+            public void onChanged(List<QuizListModel> quizListModels) {
+                //load Recyclerview
+                listView.startAnimation(fadeInAnim);
+                listProgress.startAnimation(fadeOutAnim);
+
+                adapter.setQuizListModels(quizListModels);
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+    @Override
+    public void onItemClicked(int position) {
+        ListFragmentDirections.ActionListFragmentToDetailsFragment action = ListFragmentDirections.actionListFragmentToDetailsFragment();
+        action.setPosition(position);
+            navController.navigate(action);
     }
 }
